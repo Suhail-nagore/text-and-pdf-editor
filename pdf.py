084a98adf84b97b8ccce0064220ab1cf8653c093
Mohd Suhail <118762245+Suhail-nagore@users.noreply.github.com>
2023-04-14 12:55:29 +0530

    Add files via upload

---
+++ pdf.py
@@ -0,0 +1,10 @@
+a = int(input())
+
+if(a==1):
+    print("this choice is not available")
+
+if(a==2):
+    print("press 1 for pdf in speech")
+    d=int(input())
+    if(d==1):
+        print("enter the file name\n")
